b2b4abbaa557ba009888a16be826875470185642
nando <castill0jcz@hotmail.com>
2025-11-14 03:31:02 -0500

    asteroid spawner completed

---
+++ asteroids_clone/Scripts/asteroid_spawner.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var window_size = Vector2(1024,768)
+var location = Vector2()
+
+const ASTEROID_BIG = preload("res://Scenes/asteroid_big.tscn")
+
+var top_screen = 81
+var bot_screen = 715
+var left_screen = 120
+var right_screen = 950
+
+var asteroid_count = 5
+
+var is_stage_completed = false
+var spawns = ["top","bot","left","right"]
+
+func _process(delta: float) -> void:
+	if is_stage_completed == true:
+		spawn_asteroids()
+
+func spawn_asteroids():
+	randomize()
+	
+	for asteroid in range(0, asteroid_count):
+		var key = spawns.pick_random()
+		match key:
+			"top":
+				location.x = randi_range(left_screen,right_screen)
+				location.y = top_screen
+			"bot":
+				location.x = randi_range(left_screen, right_screen)
+				location.y = bot_screen
+			"left":
+				location.x = left_screen 
+				location.y = randi_range(top_screen, bot_screen)
+			"right":
+				location.x = right_screen
+				location.y = randi_range(top_screen, bot_screen)
+		var new_asteroid = ASTEROID_BIG.instantiate()
+		new_asteroid.position = location
+		add_child(new_asteroid)
+		is_stage_completed = false
--- asteroids_clone/Scripts/game_manager.gd
+++ asteroids_clone/Scripts/game_manager.gd
@@ -6,12 +6,17 @@ var player = PLAYER.instantiate()
 var player_default_x = 505.0
 var player_defaul_y = 423.0
 
+const ASTEROID_SPAWNER = preload("res://Scenes/asteroid_spawner.tscn")
+
 func _ready() -> void:
 	player.position.x = player_default_x
 	player.position.y = player_defaul_y
 	add_child(player)
 	player.died.connect(respawn)
-
+	
+	var asteroid_spawner = ASTEROID_SPAWNER.instantiate()
+	asteroid_spawner.is_stage_completed = true
+	add_child(asteroid_spawner)
 
 func respawn():
 	player.position.x = player_default_x
